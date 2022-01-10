e61d3c7e726ce846af2c7cedc5feef86eb42345e
Kamil Zazula <zazi124@op.pl>
2022-01-10 16:04:02 +0100

    9 - Admin Panel

--- Blog_2/Controllers/PanelController.cs
+++ Blog_2/Controllers/PanelController.cs
@@ -2,9 +2,6 @@
 using Blog_2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Blog_2.Controllers
