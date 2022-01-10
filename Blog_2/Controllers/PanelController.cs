e2a959f7bc15b41ac919c142953f7d6d348bc952
Kamil Zazula <zazi124@op.pl>
2022-01-10 19:17:41 +0100

    12 - Javascript Text Editor

--- Blog_2/Controllers/PanelController.cs
+++ Blog_2/Controllers/PanelController.cs
@@ -40,7 +40,8 @@ namespace Blog_2.Controllers
                 {
                     Id = post.Id,
                     Title = post.Title,
-                    Body = post.Body
+                    Body = post.Body,
+                    CurrentImage = post.Image
                 });
             }
         }
@@ -55,6 +56,15 @@ namespace Blog_2.Controllers
                 Body = vm.Body,
                 Image = await _fileManager.SaveImage(vm.Image)
             };
+
+            if(vm.Image == null)
+            {
+                post.Image = vm.CurrentImage;
+            }
+            else
+            {
+                post.Image = await _fileManager.SaveImage(vm.Image);
+            }
 
             if (post.Id > 0)
             {
